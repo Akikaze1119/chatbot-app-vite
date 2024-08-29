5a9c154b540a31fab29e3ac5b87449a56a40f936
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2024-08-28 23:53:22 -0700

    Update router.js to test api

--- server/_dist/routers/router.js
+++ server/_dist/routers/router.js
@@ -7,4 +7,8 @@ router.post('/chats', startChat);
 router.post('/chats/restart', restartChat);
 // Messages
 router.post('/messages', sendMessage);
+// Test
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 export default router;
